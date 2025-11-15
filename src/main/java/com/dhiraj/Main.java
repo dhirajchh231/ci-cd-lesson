b8f4485581af9aa1925c9186d8c61be9bec1b223
Dhiraj <dhirajchhetri231@gmail.com>
2025-11-15 01:15:46 -0600

    Fixed import

--- src/main/java/com/dhiraj/Main.java
+++ src/main/java/com/dhiraj/Main.java
@@ -2,6 +2,7 @@ package com.dhiraj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @RestController
